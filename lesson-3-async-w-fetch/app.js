2264c7be1a5da0f2c9049c3a5af0e01874fa0d80
Valeria <valery0007@gmail.com>
2018-06-08 21:24:05 +0200

    fetch lesson3

--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -9,20 +9,28 @@
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
 
-
-
-
-
-        fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
-    headers: {
-        Authorization: 'Client-ID abc123'
+    fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
+    	headers: {
+        	Authorization: 'Client-ID c365e98e5d2a4134a09c4ebe259be3bf49b5081e8ee932cdf29db4a8a1053faa'
     }
 }).then(response => response.json())
 .then(addImage)
 .catch(e => requestError(e, 'image'));
 
 function addImage(data) {
-    
+	let htmlContent = '';
+	const firstImage = data.results[0];
+
+	if(firstImage) {
+		htmlContent = `<figure>
+		<img src="${firstImage.urls.small}" alt="${searchedForText}">
+		<figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+		</figure>`;
+	} else {
+		htmlContent = 'Unfortunately, no image was returned for your search.'
+	}
+
+	responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
 }
 
 function requestError(e, part) {
@@ -31,36 +39,15 @@ function requestError(e, part) {
 }
 
 
-
-fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}`) 
+fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=d5bc16eb1d774a15bea3170d7d577517`,{
+	headers: {
+		Authorization: 'Client-ID d5bc16eb1d774a15bea3170d7d577517'
+	}
+}) 
 .then(response => response.json())
 .then(addArticles)
 .catch(err => requestError(err, 'articles'));
-
-
-
     });
-
-
-function addImage(){
-	let htmlContent = '';
-	const data = JSON.parse(this.responseText);
-
-	if(data && data.results && data.results[0]){
-		const firstImage = data.results[0];
-
-		htmlContent = `<figure> 
-				<img src="${firstImage.urls.regular}" 
-				alt ="${searchedForText}">
-				<figcaption>${searchedForText} by ${firstImage.user.name}
-				</figcaption>
-				</figure>`;
-	} else {
-		htmlContent = '<div class= "error-no-image">No images available</div>';
-	}
-	responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
-
-}
 
 function addArticles (data) {
 	let htmlContent = '';
@@ -76,6 +63,6 @@ function addArticles (data) {
 		htmlContent = '<div class="error-no-articles">No articles available</div>';
 	}
 	responseContainer.insertAdjacentHTML('beforeend', htmlContent);
-
 }
+
 })();
